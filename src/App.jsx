c30f30de9c75955562673606c8c9f5c85a7bc964
romanfaria <roman.faria@gmail.com>
2023-06-27 15:02:54 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route, useParams } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>Homepage</h1>
+    </>
+  );
+}
+
+export default App;
