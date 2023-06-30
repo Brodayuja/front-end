e62f1643e2ae53c96e9469a279e6c16978207aa1
dculbert89 <dculbert89@gmail.com>
2023-06-30 16:59:52 -0400

    linstalled tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Routes, Route, useParams } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
+import { fetchAllBooks } from "./components/api-handlers";
 
 
 
 
 function App() {
+    const [books, setBooks] = useState([]);
 
-
+    useEffect(()=>{
+        const getBooks = async () => {
+            try {
+                const result = await fetchAllBooks();
+                setBooks(result);
+                console.log(result)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getBooks();
+    }, [])
 
   return (
     <>
 
-    <LandingPage/>
+    <Routes>
+      <Route path='/' element={<LandingPage books={books}/>}></Route>
+      {/* <Route path='/login' element={<Login/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
+      <Route path='/search-results' element={<SearchResults/>}></Route> */}
+    </Routes>
       
     
     </>
--- src/components/Reviews/ReviewBookShelf.jsx
+++ src/components/Reviews/ReviewBookShelf.jsx
@@ -1,47 +1,32 @@
 import { useState, useEffect } from "react";
 import { fetchAllBooks } from "../api-handlers";
 
-function ReviewBookShelf () {
-    const [books, setBooks] = useState([]);
-
-    useEffect(()=>{
-        const getBooks = async () => {
-            try {
-                const result = await fetchAllBooks();
-                setBooks(result);
-                console.log(result)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        getBooks();
-    }, [])
-
-
-
+function ReviewBookShelf (props) {
+    const books = props.books;
+    
     return(
-        <>
-    <h2>I am reviews</h2>
-    {/* {console.log(books)} */}
 
-        <div className="flex-row">
-            {books.length ? (
-                books.map((element) => (
-                    <div key={element.ISBN}>
+            <div>
+                <h2 className="text-left">New Releases</h2>
+                <div className="flex flex-row justify-between">
+                    {books.length ? (
+                        books.map((element) => (
+                           
+                            <div key={element.ISBN} className="w-1/4 px-2 bg-columbiaBlue">
 
-                        <img src={element.bookCover} alt="Image of Book cover"></img>
-                        <h4>{element.title}</h4>
-                        {/* Need to be able to render Score Here*/}
-                        
-                    </div>
-                ))
-            ) : (
-                <p>Loading . . .</p>
-            )}
+                                <img src={element.bookCover} alt="Image of Book cover"></img>
+                                {/* <h4>{element.title}</h4> */}
+                                {/* Need to be able to render Score Here*/}
+                                
+                            </div>
+                        ))
+                    ) : (
+                        <p>Loading . . .</p>
+                    )}
+
+                </div>
 
         </div>
-
-        </>
     )
 }
 
