46415d94f95b886363d7f3a25ec44e6954234fc8
romanfaria <roman.faria@gmail.com>
2023-07-03 13:32:50 -0400

    added login/registration components to app.jsx and landingpage

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,55 +1,58 @@
 import { useState } from "react";
 import { Routes, Route, useParams } from "react-router-dom";
-import pageTurnerLogo from "../images/pageTurnersLogo.png"
+import pageTurnerLogo from "../images/pageTurnersLogo.png";
 import SearchBar from "../SearchBar/SearchBar";
 import RecentReviewsShelf from "../Reviews/RecentReviewsShelf";
+import Login from "../Login/Login";
 
 // commented out to keep from breaking
-
-
-
 
 function LandingPage(props) {
   const books = props.books;
 
-
   return (
     <>
-    <div>
-      <img className="Logo" src={pageTurnerLogo}/>
-    </div>
-  
-    <div>
-      <h1>Page Turners</h1>
-      {/* Background Header Img */}
-    </div>
+      <div>
+        <img className="Logo" src={pageTurnerLogo} />
+      </div>
 
-    <div>
-      {/* Log in Component */}
-    </div>
+      <div>
+        <h1>Page Turners</h1>
+        {/* Background Header Img */}
+      </div>
 
-    <article>
-      <p><b>Deciding what to read next?</b> <br></br> You’re in the right place. Tell us what titles or genres you’ve enjoyed in the past, and we’ll give you surprisingly insightful recommendations. </p>
+      <div>
+        <Login />
+      </div>
 
-      <p><b>What are your friends reading?</b> <br></br> Chances are your friends are discussing their favorite (and least favorite) books on Page Turners. </p>
-    </article>
+      <article>
+        <p>
+          <b>Deciding what to read next?</b> <br></br> You’re in the right
+          place. Tell us what titles or genres you’ve enjoyed in the past, and
+          we’ll give you surprisingly insightful recommendations.{" "}
+        </p>
 
-    <div>
-     <RecentReviewsShelf books={books}/>
-    </div>
+        <p>
+          <b>What are your friends reading?</b> <br></br> Chances are your
+          friends are discussing their favorite (and least favorite) books on
+          Page Turners.{" "}
+        </p>
+      </article>
 
-    <div>
-      {/* commented out to keep from breaking */}
-      <SearchBar books={books}/>
-    </div>
+      <div>
+        <RecentReviewsShelf books={books} />
+      </div>
 
-    <div>
-      {/* Global Search Companent */}
+      <div>
+        {/* commented out to keep from breaking */}
+        <SearchBar books={books} />
+      </div>
 
-      {/* Links to Categories */}
-    </div>
-    
-    
+      <div>
+        {/* Global Search Companent */}
+
+        {/* Links to Categories */}
+      </div>
     </>
   );
 }
