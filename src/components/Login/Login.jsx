af4ecc143b5e0666724d89c0511267734cc90b26
romanfaria <roman.faria@gmail.com>
2023-07-03 12:53:15 -0400

    added Login/register api handlers. added login and register components

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,58 @@
+import { loginUser } from "../api-handlers";
+import { useNavigate, Link } from "react-router-dom";
+import React, { useState } from "react";
+
+const Login = (props) => {
+  const setIsLoggedIn = props.setIsLoggedIn;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const setMyUsername = props.setMyUsername;
+
+  const handeSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const result = await loginUser(username, password);
+      console.log(result);
+
+      localStorage.setItem("token", result.token);
+      setIsLoggedIn(true);
+      setMyUsername(username);
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handeSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <Link to="/register">Don't have an account? Click Here!</Link>
+    </div>
+  );
+};
--- src/components/api-handlers/index.js
+++ src/components/api-handlers/index.js
@@ -1,44 +1,94 @@
-const BASE_URL = "http://localhost:3000/api" 
-
+const BASE_URL = "http://localhost:3000/api";
 
 // Fetch All Books
 export const fetchAllBooks = async () => {
-    try {
-        const nfResponse = await fetch(`${BASE_URL}/nonfiction-books`)
-        const nfData = await nfResponse.json();
+  try {
+    const nfResponse = await fetch(`${BASE_URL}/nonfiction-books`);
+    const nfData = await nfResponse.json();
 
-        const ficResponse = await fetch(`${BASE_URL}/fiction-books`)
-        const ficData = await ficResponse.json();
+    const ficResponse = await fetch(`${BASE_URL}/fiction-books`);
+    const ficData = await ficResponse.json();
 
-        const gnResponse = await fetch(`${BASE_URL}/graphic-books`)
-        const gnData = await gnResponse.json();
+    const gnResponse = await fetch(`${BASE_URL}/graphic-books`);
+    const gnData = await gnResponse.json();
 
-        const clubResponse = await fetch(`${BASE_URL}/book-club-picks`)
-        const clubData = await clubResponse.json();
+    const clubResponse = await fetch(`${BASE_URL}/book-club-picks`);
+    const clubData = await clubResponse.json();
 
-        const childResponse = await fetch(`${BASE_URL}/childrens-books`)
-        const childData = await childResponse.json();
+    const childResponse = await fetch(`${BASE_URL}/childrens-books`);
+    const childData = await childResponse.json();
 
-        const allBooks = [].concat(...nfData, ...ficData, ...gnData, ...clubData, ...childData)
+    const allBooks = [].concat(
+      ...nfData,
+      ...ficData,
+      ...gnData,
+      ...clubData,
+      ...childData
+    );
 
-        return allBooks;
+    return allBooks;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-fetchAllBooks()
+fetchAllBooks();
 
 // Fetch Reviews
 export const fetchReviews = async () => {
-    try {
-        const response = await fetch(`${BASE_URL}/reviews`)
-        const data = await response.json();
+  try {
+    const response = await fetch(`${BASE_URL}/reviews`);
+    const data = await response.json();
 
-        return data;
-    } catch (error) {
-        console.log(error)
-    }
-}
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+// User login
+export const loginUser = async (username, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// User Registration
+export const registerUser = async (username, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password,
+        },
+      }),
+    });
+
+    const result = await response.json();
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
