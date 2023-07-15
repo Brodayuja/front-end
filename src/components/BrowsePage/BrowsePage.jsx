cbb1610273025ad75afbac1d1642d098932a47f3
romanfaria <roman.faria@gmail.com>
2023-07-15 15:04:54 -0400

    got google auth route working

--- src/components/BrowsePage/BrowsePage.jsx
+++ src/components/BrowsePage/BrowsePage.jsx
@@ -5,36 +5,46 @@ import NavBar from "../NavBar/NavBar";
 import RecentReviewsShelf from "../Reviews/RecentReviewsShelf";
 import GraphicNovelsShelf from "../GraphicNovels/GraphicNovelsShelf";
 import ChildrensBooksShelf from "../ChildrensBooks/ChildrensBooksShelf";
+import Cookies from "js-cookies";
 
-function Browse ({books, averageScores}) {
-    
-    return (
-        <>
-        <div className="flex justify-between">
-            <img className="Logo" src={pageTurnerLogo} alt="Page Turner Logo" />
-            <NavBar/>
-        </div>
+function Browse({ books, averageScores, setMyUserId }) {
+  const myToken = localStorage.getItem("token");
 
+  if (!myToken) {
+    const handleGoogleSuccess = () => {
+      const cookieToken = Cookies.getItem("token");
+      const cookieUsername = Cookies.getItem("username");
+      const cookieId = Cookies.getItem("id");
+      localStorage.setItem("token", cookieToken);
+      localStorage.setItem("username", cookieUsername);
+      localStorage.setItem("id", cookieId);
+      setMyUserId(cookieId);
+    };
+    handleGoogleSuccess();
+  }
 
-        <div>
-            <p>This is Browse</p>
+  return (
+    <>
+      <div className="flex justify-between">
+        <img className="Logo" src={pageTurnerLogo} alt="Page Turner Logo" />
+        <NavBar />
+      </div>
 
-            <RecentReviewsShelf books={books} averageScores={averageScores}/>
+      <div>
+        <p>This is Browse</p>
 
-            <FictionShelf averageScores={averageScores}/>
+        <RecentReviewsShelf books={books} averageScores={averageScores} />
 
-            <NFBooksShelf averageScores={averageScores}/>
+        <FictionShelf averageScores={averageScores} />
 
-            <GraphicNovelsShelf averageScores={averageScores}/>
+        <NFBooksShelf averageScores={averageScores} />
 
-            <ChildrensBooksShelf averageScores={averageScores}/>
+        <GraphicNovelsShelf averageScores={averageScores} />
 
-            
-
-
-        </div>
-</>
-    )
+        <ChildrensBooksShelf averageScores={averageScores} />
+      </div>
+    </>
+  );
 }
 
 export default Browse;
