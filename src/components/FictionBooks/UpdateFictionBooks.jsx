cbf7bd80ced21859a21c1d15f252ef5bf5740093
jcassidy3280 <jcassidy3280@gmail.com>
2023-07-16 18:03:26 -0400

    update books initial

--- src/components/FictionBooks/UpdateFictionBooks.jsx
+++ src/components/FictionBooks/UpdateFictionBooks.jsx
@@ -1,240 +1,66 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react"
-import { useParams } from "react-router-dom"
-import { BASE_URL } from "../api-handlers/index"
+import { UpdateFiction } from "../UpdateBooks/UpdateFiction"
+import { UpdateNonfiction } from "../UpdateBooks/UpdateNonfiction"
+import { UpdateChildrens } from "../UpdateBooks/UpdateChildrens"
+import { UpdateGraphicNovel } from "../UpdateBooks/UpdateGraphicNovel"
+import { UpdateBookClubPick } from "../UpdateBooks/UpdateBookClubPick"
 
-const UpdateFictionBook = ({books, setBooks}) => {
 
-    const [newTitle, setNewTitle] = useState("")
-    const [newAuthor, setNewAuthor] = useState("")
-    const [newSummary, setNewSummary] = useState("")
-    const [newGenre, setNewGenre] = useState([])
-    const [newPublisher, setNewPublisher] = useState("")
-    const [newYearPublished, setNewYearPublished] = useState("")
-    const [newPhysicalDescription, setNewPhysicalDescription] = useState("")
-    const [newBookCover, setNewBookCover] = useState("")
+const UpdateFictionBook = () => {
+    const [category, setCategory] = useState("")
     const currentToken = localStorage.getItem("token")
-    const {isbn} = useParams()
 
-    const sendBookUpdates = async (event) => {
-        event.preventDefault()
-        const updatedBook = {}
-
-
-
-        if (newSummary) {
-            updatedBook.summary = newSummary
-        }
-        if (newGenre.length) {
-            updatedBook.genre = newGenre
-        }
-        if (newPublisher) {
-            updatedBook.publisher = newPublisher
-        }
-        if (newYearPublished) {
-            updatedBook.yearPublished = newYearPublished
-        }
-        if (newPhysicalDescription) {
-            updatedBook.physicalDescription = newPhysicalDescription
-        }
-
-        try {
-            const response = await fetch(`${BASE_URL}/fiction-books/${isbn}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${currentToken}`
-                }, 
-                body: JSON.stringify(updatedBook)
-            });
-            
-            const data = await response.json();
-            const booksWithNoChange = books.filter((e) => { 
-              console.log(e.isbn, typeof e.isbn)
-              return (e.isbn !== isbn)
-            })
-            console.log( data, booksWithNoChange)
-            setBooks([...booksWithNoChange, data])
-          } catch (error) {
-            console.log (error)
-          }
-        }
-
-        return (
+  return (
       <>
         <h2><b>Update Book:</b></h2>
-        <form onSubmit={sendBookUpdates}>
-
-          <label htmlFor="summary">Enter Book Summary Below:</label>
-          <br />
-          <input
-            name="summary"
-            type="textarea"
-            placeholder="New Book Summary Goes Here"
-            value={newSummary}
-            onChange={(event) => {
-              setNewSummary(event.target.value);
+        <form >
+          <label htmlFor="category">Select category: </label>
+          <select id="category" name="category">
+            <option></option>
+            <option value="Nonfiction Book"
+            onChange={(event)=>{
+              setCategory(event.target.value)
             }}
-          ></input> <br />
-
-          <fieldset>
-              <legend>Book Genres:</legend>
-              <div>
-                <label htmlFor="genre">Contemporary Fiction</label>
-                <input 
-                  type="checkbox" 
-                  id="genre" 
-                  name="genre" 
-                  value="Contemporary Fiction"
-                  onChange={(event)=> {
-                    setNewGenre([...newGenre, event.target.value])
-                  }
-                  }>
-                  </input> 
-              </div>
-              <div>
-                <label htmlFor="genre">Fantasy</label>
-                <input 
-                  type="checkbox" 
-                  id="genre" 
-                  name="genre" 
-                  value="Fantasy"
-                  onChange={(event)=> {
-                    setNewGenre([...newGenre, event.target.value])
-                  }
-                  }>
-                  </input> 
-              </div>
-              <div>
-                <label htmlFor="genre">Historical Fiction</label>
-                <input 
-                  type="checkbox" 
-                  id="genre" 
-                  name="genre" 
-                  value="Historical Fiction"
-                  onChange={(event)=> {
-                    setNewGenre([...newGenre, event.target.value])
-                  }
-                  }>
-                  </input> 
-              </div>
-              <div>
-                <label htmlFor="genre">Horror</label>
-                <input 
-                  type="checkbox" 
-                  id="genre" 
-                  name="genre" 
-                  value="Horror"
-                  onChange={(event)=> {
-                    setNewGenre([...newGenre, event.target.value])
-                  }
-                  }>
-                  </input> 
-              </div>
-              <div>
-                <label htmlFor="genre">LGBTQ+</label>
-                <input 
-                  type="checkbox" 
-                  id="genre" 
-                  name="genre" 
-                  value="LGBTQ+"
-                  onChange={(event)=> {
-                    setNewGenre([...newGenre, event.target.value])
-                  }
-                  }>
-                  </input> 
-              </div>
-              <div>
-                <label htmlFor="genre">Mystery</label>
-                <input 
-                  type="checkbox" 
-                  id="genre" 
-                  name="genre" 
-                  value="Mystery"
-                  onChange={(event)=> {
-                    setNewGenre([...newGenre, event.target.value])
-                  }
-                  }>
-                  </input> 
-              </div>
-              <div>
-                <label htmlFor="genre">Romance</label>
-                <input 
-                  type="checkbox" 
-                  id="genre" 
-                  name="genre" 
-                  value="Romance"
-                  onChange={(event)=> {
-                    setNewGenre([...newGenre, event.target.value])
-                  }
-                  }>
-                  </input> 
-              </div>
-              <div>
-                <label htmlFor="genre">Science Fiction</label>
-                <input 
-                  type="checkbox" 
-                  id="genre" 
-                  name="genre" 
-                  value="Science Fiction"
-                  onChange={(event)=> {
-                    setNewGenre([...newGenre, event.target.value])
-                  }
-                  }>
-                  </input> 
-              </div>
-              <div>
-                <label htmlFor="genre">Short Story </label>
-                <input 
-                  type="checkbox" 
-                  id="genre" 
-                  name="genre" 
-                  value="Short Story"
-                  onChange={(event)=> {
-                    setNewGenre([...newGenre, event.target.value])
-                  }
-                  }>
-                  </input> 
-              </div>
-          </fieldset>
-
-          <label htmlFor="publisher">Enter Book Publisher:</label>
-          <br />
-          <input
-            name="publisher"
-            type="text"
-            placeholder="example: Random House"
-            value={newPublisher}
-            onChange={(event) => {
-              setNewPublisher(event.target.value);
+            >Nonfiction Book</option>
+            <option value="Fiction Book"
+            onChange={(event)=>{
+              setCategory(event.target.value)
             }}
-          ></input> <br />
-
-          <label htmlFor="yearPublished">Enter Year of Publication:</label>
-          <br />
-          <input
-            name="yearPublished"
-            type="text"
-            placeholder="ex. 2023"
-            value={newYearPublished}
-            onChange={(event) => {
-              setNewYearPublished(event.target.value);
+            >Fiction Book</option>
+            <option value="Graphic Novel/Manga"
+            onChange={(event)=>{
+              setCategory(event.target.value)
             }}
-          ></input> <br />
-          <label htmlFor="physicalDescription">Enter Number of Pages:</label>
-          <br />
-          <input
-            name="physcialDescription"
-            type="text"
-            placeholder="ex. 388 p."
-            value={newPhysicalDescription}
-            onChange={(event) => {
-              setNewPhysicalDescription(event.target.value);
+            >Graphic Novel/Manga</option>
+            <option value="Book Club Pick"
+            onChange={(event)=>{
+              setCategory(event.target.value)
             }}
-          ></input> <br />
-
-          <button type="submit">Update Book Details!</button>
+            >Book Club Pick</option>
+            <option value="Children's Book"
+            onChange={(event)=>{
+              setCategory(event.target.value)
+            }}
+            >Children's Book</option>
+          </select>
         </form>
+        {
+          category == "Nonfiction Book" ? <UpdateNonfiction /> : null  
+        }
+        {
+          category == "Fiction Book" ? <UpdateFiction /> : null  
+        }
+        {
+          category == "Children's Book" ? <UpdateChildrens /> : null
+        }
+        {
+          category == "Book Club Pick" ? <UpdateBookClubPick /> : null
+        }
+        {
+          category == "Graphic Novel/Manga" ? <UpdateGraphicNovel /> : null
+        }
+       
       </>
     );
 }
