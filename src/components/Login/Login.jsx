a5962c6666f1085ceb58c2291a1ed077bd8385f2
romanfaria <roman.faria@gmail.com>
2023-07-18 15:44:03 -0400

    added google icon to login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { fetchAllUserData, loginUser } from "../api-handlers";
 import { useNavigate, Link } from "react-router-dom";
 import React, { useState } from "react";
+import GoogleIcon from "@mui/icons-material/Google";
 
 const Login = (props) => {
   const setIsLoggedIn = props.setIsLoggedIn;
@@ -18,12 +19,10 @@ const Login = (props) => {
       setIsLoggedIn(true);
       setMyUsername(username);
 
-
       localStorage.setItem("token", result.token);
       localStorage.setItem("userId", result.id);
       localStorage.setItem("username", username);
       const myToken = result.token;
-
 
       if (myToken) {
         const fetchUsers = await fetchAllUserData();
@@ -47,47 +46,40 @@ const Login = (props) => {
 
   return (
     <div className="flex flex-col items-center justify-center text-black">
-
-            <h1 className="text-center">Login</h1>
-
-            <br/>
-
-            <form onSubmit={handleSubmit} className="flex flex-col items-center">
-              <div className="flex items-center">
-                <label>
-                  Username:
-                  <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => {
-                      setUsername(e.target.value);
-                    }}
-                  />
-                </label>
-              </div>
-
-              <br/>
-
-              <div className="flex items-center">
-                <label>
-                  Password:
-                  <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
-                  />
-                </label>
-              </div>
-
-              <button type="submit">Login</button>
-            </form>
-
-
-
+      <h1 className="text-center">Login</h1>
+      <br />
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
+        <div className="flex items-center">
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </label>
+        </div>
+        <br />
+        <div className="flex items-center">
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </label>
+        </div>
+        <button type="submit">Login</button>
+        or
+      </form>
 
       <button type="button" onClick={handleGoogleLogin}>
+        <GoogleIcon className="pr-1" />
         Login with Google
       </button>
       <Link to="/register">Don't have an account? Click Here!</Link>
