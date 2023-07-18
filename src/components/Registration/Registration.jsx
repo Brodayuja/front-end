ba54bcf6882025edcad61ff5c6d56287367fe0ef
okcbatt <okcbatt405@gmail.com>
2023-07-18 15:11:13 -0500

    registration styling

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-import { registerUser } from "../api-handlers/index";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { registerUser } from '../api-handlers/index';
+import { useNavigate } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
 
 const Register = (props) => {
   const setIsLoggedIn = props.setIsLoggedIn;
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const navigate = useNavigate();
   const setMyUsername = props.setMyUsername;
 
@@ -17,66 +18,76 @@ const Register = (props) => {
     try {
       const result = await registerUser(username, password, email, name);
 
-
-      localStorage.setItem("token", result.token);
-      localStorage.setItem("userId", result.id)
-      localStorage.setItem("username", username)
+      localStorage.setItem('token', result.token);
+      localStorage.setItem('userId', result.id);
+      localStorage.setItem('username', username);
       setIsLoggedIn(true);
       setMyUsername(username);
 
-      navigate("/browse");
+      navigate('/browse');
     } catch (error) {
       console.log(error);
     }
   };
+
   return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </label>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-        </label>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <>
+      <NavBar />
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="bg-columbiaBlue p-6 border text-black w-1/3 rounded-xl">
+          <h2 className="text-2xl font-semibold mb-6 text-center">Create Account</h2>
+          <form onSubmit={handleSubmit} className="grid gap-4">
+            <label>
+              Email:
+              <input
+                className="w-full border rounded-lg px-4 py-2"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label>
+              Name:
+              <input
+                className="w-full border rounded-lg px-4 py-2"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+            <label>
+              Username:
+              <input
+                className="w-full border rounded-lg px-4 py-2"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <label>
+              Password:
+              <input
+                className="w-full border rounded-lg px-4 py-2"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+              >
+                Create
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default Register;
+
+
+
